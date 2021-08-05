a5c03aaada30ed345f0129956d905ce248a0a9d9
Lencof <71154457+Lencof@users.noreply.github.com>
2021-08-05 08:40:16 +0300

    Create sorted_nums.py

---
+++ sorted_nums.py
@@ -0,0 +1,15 @@
+# __Author__ __Lencof__
+# Qos.py
+
+def sorted(nums):
+    s = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+
+random_list_of_nums = [5, 2, 1, 4, 6]
+sorted(random_list_of_nums)
+print(random_list_of_nums)
